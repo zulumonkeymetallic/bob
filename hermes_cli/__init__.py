e08778fa1ee377f7128641f8cc03b0de046bd8da
Teknium <127238744+teknium1@users.noreply.github.com>
2026-03-30 08:29:38 -0700

    chore: release v0.6.0 (2026.3.30) (#3985)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -11,5 +11,5 @@ Provides subcommands for:
 - hermes cron          - Manage cron jobs
 """
 
-__version__ = "0.5.0"
-__release_date__ = "2026.3.28"
+__version__ = "0.6.0"
+__release_date__ = "2026.3.30"
