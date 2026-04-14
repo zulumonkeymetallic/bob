902f1e6ede20dd618d64aa6dccced966675f8316
MestreY0d4-Uninter <241404605+MestreY0d4-Uninter@users.noreply.github.com>
2026-04-14 17:19:55 +0000

    chore: add MestreY0d4-Uninter to AUTHOR_MAP and .mailmap

--- scripts/release.py
+++ scripts/release.py
@@ -63,6 +63,7 @@ AUTHOR_MAP = {
     "70424851+insecurejezza@users.noreply.github.com": "insecurejezza",
     "259807879+Bartok9@users.noreply.github.com": "Bartok9",
     "268667990+Roy-oss1@users.noreply.github.com": "Roy-oss1",
+    "241404605+MestreY0d4-Uninter@users.noreply.github.com": "MestreY0d4-Uninter",
     # contributors (manual mapping from git names)
     "dmayhem93@gmail.com": "dmahan93",
     "samherring99@gmail.com": "samherring99",
