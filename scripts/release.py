9bbf7659e98928d551fb1b7a61020d4c694953ae
Teknium <teknium1@gmail.com>
2026-04-14 14:19:49 -0700

    chore: add Roy-oss1 to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -62,6 +62,7 @@ AUTHOR_MAP = {
     "258577966+voidborne-d@users.noreply.github.com": "voidborne-d",
     "70424851+insecurejezza@users.noreply.github.com": "insecurejezza",
     "259807879+Bartok9@users.noreply.github.com": "Bartok9",
+    "268667990+Roy-oss1@users.noreply.github.com": "Roy-oss1",
     # contributors (manual mapping from git names)
     "dmayhem93@gmail.com": "dmahan93",
     "samherring99@gmail.com": "samherring99",
