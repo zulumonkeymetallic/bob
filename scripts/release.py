764536b684b081eead7f4394911b4399a66e7f9c
Teknium <teknium1@gmail.com>
2026-04-16 16:49:42 -0700

    chore(release): map mbelleau@Michels-MacBook-Pro.local to @malaiwah
    Follow-up for #11272 so release notes attribute the RTP padding fix correctly.

--- scripts/release.py
+++ scripts/release.py
@@ -227,6 +227,7 @@ AUTHOR_MAP = {
     "zzn+pa@zzn.im": "xinbenlv",
     "zaynjarvis@gmail.com": "ZaynJarvis",
     "zhiheng.liu@bytedance.com": "ZaynJarvis",
+    "mbelleau@Michels-MacBook-Pro.local": "malaiwah",
 }
 
 
